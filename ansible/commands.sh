09e9f0163cf30494dc776c62df185606119ee40d
PauloBusch <paulo202015@outlook.com.br>
2024-06-02 17:54:12 -0300

    feat: run multiple playbooks

--- ansible/commands.sh
+++ ansible/commands.sh
@@ -10,3 +10,11 @@ ansible-playbook playbooks/03-variables.yml
 ansible-playbook playbooks/03-variables.yml -e variable1=CLI_Value
 ansible-playbook playbooks/04-facts.yml
 ansible-playbook playbooks/05-install-apache.yml
+
+# Multiple playbooks
+ansible-playbook playbooks/06-multiple-playbooks.yml
+ansible-playbook playbooks/06-multiple-playbooks.yml --list-tasks
+ansible-playbook playbooks/06-multiple-playbooks.yml --list-hosts
+
+# Runs only in qa set
+ansible-playbook -l qa playbooks/01-ping.yml
